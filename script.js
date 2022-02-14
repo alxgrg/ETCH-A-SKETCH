a7fae61a6ff896eda2b5ecad7bd1e402cb5929ba
alxgrg <alxmgrg@gmail.com>
2022-02-14 02:25:17 -0500

    retool functions

--- script.js
+++ script.js
@@ -1,34 +1,62 @@
 let grid = document.getElementById('grid');
-let button = document.getElementById('new-sketch');
+let clearButton = document.getElementById('new-sketch');
+let gridSizeForm = document.getElementById('grid-form');
 
 buildGrid();
 
 let cells = document.getElementsByClassName('cell');
-cells = [...cells];
+initCells();
 
-cells.map((cell) => {
-  cell.addEventListener('mouseover', () => {
-    cell.classList.add('sketched');
+mapListeners();
+
+function initCells() {
+  cells = document.getElementsByClassName('cell');
+  cells = [...cells];
+}
+
+function mapListeners() {
+  cells.map((cell) => {
+    cell.addEventListener('mouseover', () => {
+      let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+      cell.style.backgroundColor = '#' + randomColor;
+    });
   });
-});
+}
 
 function buildGrid(rows = 16, cols = 16) {
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
+  grid.innerHTML = '';
+  for (let i = 0; i < cols; i++) {
+    let col = document.createElement('div');
+    for (let j = 0; j < rows; j++) {
       const cell = document.createElement('div');
       cell.className = 'cell';
       cell.textContent = '';
-      grid.appendChild(cell);
+      col.appendChild(cell);
     }
+    grid.appendChild(col);
   }
 }
 
-function clearGrid() {
+function handleGridSizeChange(rows, cols) {
+  buildGrid(rows, cols);
+}
+
+function handleClearGrid() {
   cells.forEach((cell) => {
     cell.className = 'cell';
+    cell.style = '';
   });
 }
 
-button.addEventListener('click', () => {
-  clearGrid();
+clearButton.addEventListener('click', () => {
+  handleClearGrid();
+});
+
+gridSizeForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  let rows = event.target.elements['rows'].value;
+  let cols = event.target.elements['cols'].value;
+  handleGridSizeChange(rows, cols);
+  initCells();
+  mapListeners();
 });
