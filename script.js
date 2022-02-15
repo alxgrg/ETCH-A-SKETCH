e2cb152e2e3a380fad6cefc1ca742491ee17fd24
alxgrg <alxmgrg@gmail.com>
2022-02-14 23:39:37 -0500

    add slider and eraser

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let grid = document.getElementById('grid');
 let clearButton = document.getElementById('new-sketch');
-let gridSizeForm = document.getElementById('grid-form');
+let gridSize = document.getElementById('size');
+let pen = document.querySelector('.pen');
+let cellsUni = document.querySelector('.cells-uni');
+let eraser = document.querySelector('.eraser');
+let randomColor = 'random';
+let cellColor = 'black';
+let isSketching = false;
 
 buildGrid();
 
@@ -14,17 +20,49 @@ function initCells() {
   cells = [...cells];
 }
 
+// function mapListeners() {
+//   cells.map((cell) => {
+//     cell.addEventListener('mouseover', () => {
+//       if (cellColor === 'random') {
+//         cell.style.backgroundColor =
+//           '#' + Math.floor(Math.random() * 16777215).toString(16);
+//       } else {
+//         cell.style.backgroundColor = cellColor;
+//       }
+//     });
+//   });
+// }
+
 function mapListeners() {
   cells.map((cell) => {
+    cell.addEventListener('mousedown', () => {
+      isSketching = true;
+      if (cellColor === 'random') {
+        cell.style.backgroundColor =
+          '#' + Math.floor(Math.random() * 16777215).toString(16);
+      } else {
+        cell.style.backgroundColor = cellColor;
+      }
+    });
+    cell.addEventListener('mouseup', () => {
+      isSketching = false;
+    });
     cell.addEventListener('mouseover', () => {
-      let randomColor = Math.floor(Math.random() * 16777215).toString(16);
-      cell.style.backgroundColor = '#' + randomColor;
+      if (isSketching) {
+        if (cellColor === 'random') {
+          cell.style.backgroundColor =
+            '#' + Math.floor(Math.random() * 16777215).toString(16);
+        } else {
+          cell.style.backgroundColor = cellColor;
+        }
+      }
     });
   });
 }
 
-function buildGrid(size = 16) {
+function buildGrid(size = 85) {
   grid.innerHTML = '';
+  size = 101 - size;
   grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
   for (let i = 0; i < size; i++) {
@@ -49,14 +87,27 @@ function handleClearGrid() {
   });
 }
 
-clearButton.addEventListener('click', () => {
-  handleClearGrid();
-});
+function handleColorChange(color) {
+  cellColor = color;
+}
 
-gridSizeForm.addEventListener('submit', (event) => {
-  event.preventDefault();
-  let size = event.target.elements['size'].value;
+clearButton.addEventListener('click', handleClearGrid);
+
+gridSize.addEventListener('input', () => {
+  let size = gridSize.value;
   handleGridSizeChange(size);
   initCells();
   mapListeners();
+});
+
+pen.addEventListener('click', () => {
+  handleColorChange('black');
+});
+
+cellsUni.addEventListener('click', () => {
+  handleColorChange(randomColor);
+});
+
+eraser.addEventListener('click', () => {
+  handleColorChange('white');
 });
